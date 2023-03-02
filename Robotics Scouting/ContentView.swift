edf0ab8fb4dc1996d3cfcc8561c9b06c0d2bd8a6
CooperHopkins <cooper55379@gmail.com>
2023-03-02 17:06:37 -0600

    Initial Commit

---
+++ Robotics Scouting/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Robotics Scouting
+//
+//  Created by Cooper Hopkins on 3/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Robotics Scouting App")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Robotics Scouting/Robotics_ScoutingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Robotics_ScoutingApp.swift
+//  Robotics Scouting
+//
+//  Created by Cooper Hopkins on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Robotics_ScoutingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
